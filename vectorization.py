55a5222cce5bad4ea3162420a1acb04489e06cab
Peter Organisciak <organisciak@gmail.com>
2020-06-08 13:44:57 -0600

    Vectorization improvements

--- vectorization.py
+++ vectorization.py
@@ -20,12 +20,15 @@ def main():
     parser.add_argument('idlist', type=str, help='CSV file of HTIDs to process. Needs a header, with column name \'htid\'.')
     parser.add_argument('--outdir', '-o', type=str, default='data_outputs/', help='Directory to save results.')
     parser.add_argument('--chunksize', '-c', type=int, default=10000, help='Size of chunks to roll pages into.')
-    
+    parser.add_argument('--no-srp', action='store_true', help='Turn off SRP saving')
+    parser.add_argument('--no-glove', action='store_true', help='Turn off Glove saving')
     args = parser.parse_args()
     
     thread_no = args.threadno - 1 # Zero index.
+    
+    assert not (args.no_srp & args.no_glove)
 
-    already_imported = already_imported_list()
+    already_imported = already_imported_list(args.outdir)
     print("There are {} files already imported".format(len(already_imported)))
     filenames = pd.read_csv(args.idlist, low_memory = False)
     
@@ -33,16 +36,15 @@ def main():
 
     already_seen_file = open(os.path.join(args.outdir, "already_completed_files{}.csv".format(thread_name)), "a")
 
-    hasher = SRP.SRP(640)
-    out_SRP = SRP.Vector_file(os.path.join(args.outdir, thread_name + "SRP_chunks.bin"), dims=640, mode="w")
-    out_glove = SRP.Vector_file(os.path.join(args.outdir, thread_name + "Glove_chunks.bin"), dims = 300, mode="w")
+    if not args.no_srp:
+        hasher = SRP.SRP(640)
+        out_SRP = SRP.Vector_file(os.path.join(args.outdir, thread_name + "SRP_chunks.bin"), dims=640, mode="w")
+        
+        def SRP_transform(f):
+            return hasher.stable_transform(words = f['lowercase'], counts = f['count'], log = True, standardize = True)
     
-    def SRP_transform(f):
-        return hasher.stable_transform(words = f['lowercase'], counts = f['count'], log = True, standardize = True)
-
-    # Bookworm prep
-    import gzip
-    unigrams = gzip.open(os.path.join(args.outdir, thread_name + "bookworm.unigrams.gz"), "wt")
+    if not args.no_glove:
+        out_glove = SRP.Vector_file(os.path.join(args.outdir, thread_name + "Glove_chunks.bin"), dims = 300, mode="w")
 
     books = 0
     last = None
@@ -64,31 +66,26 @@ def main():
 
             id = "{}-{:04d}-{}-{}".format(id, chunk, start, end)
 
-            # Do SRP
-            SRP_rep = SRP_transform(group)
-            out_SRP.add_row(id, SRP_rep)
+            if not args.no_srp:
+                SRP_rep = SRP_transform(group)
+                out_SRP.add_row(id, SRP_rep)
 
-            # Convert to 300dim Glove vec
-            WEM_rep = WEM_transform(group)
-            out_glove.add_row(id, WEM_rep.astype('<f4'))
-
-            if False:
-                wordcount_rep = (group.reset_index(0, drop = True)
-                                      .to_csv(line_terminator="\n", header = False, index=False)
-                                      .replace("\n", "\f")
-                                )
-
-                unigrams.write("{}\t{}\n".format(id, wordcount_rep))
+            if not args.no_glove:
+                WEM_rep = WEM_transform(group)
+                out_glove.add_row(id, WEM_rep.astype('<f4'))
 
         already_seen_file.write("{}\n".format(last))
 
-        out_SRP.close()
-        out_glove.close()
-        unigrams.close()
+        if not args.no_srp:
+            out_SRP.close()
+        if not args.no_glove:
+            out_glove.close()
 
     except KeyboardInterrupt:
-        out_glove.close()
-        out_SRP.close()
+        if not args.no_srp:
+            out_SRP.close()
+        if not args.no_glove:
+            out_glove.close()
 
 
 def yielder(ids, thread_no, totalthreads, chunk_size = 10000, already_imported_list=[]):
@@ -113,7 +110,6 @@ def yielder(ids, thread_no, totalthreads, chunk_size = 10000, already_imported_l
             for (chunk, start, end) in set(chunks.index):
                 yield (id, chunk, start, end, chunks.loc[(chunk, start, end)].reset_index(drop = True))
         except:
-            raise
             print("Error chunking {}... skipping\n".format(id))
             continue
 
